6c80fa0c8937f24edf2823a45c294e881be4bab5
saisbushigampala7 <sbushigampala1@cub.uca.edu>
2023-04-17 17:38:07 -0500

    Creating BoardPanel!

---
+++ ClientGUI/BoardPanel.java
@@ -0,0 +1,21 @@
+package ClientGUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BoardPanel extends JPanel{
+
+	private JPanel boardArea;
+	private JLabel alertLabel;
+	private JLabel clockLabel;
+	private JButton resignButton;
+	private JButton drawButton;
+	private JButton alertButton;
+	
+	public BoardPanel()
+	{
+		
+	}
+	
+	
+}
