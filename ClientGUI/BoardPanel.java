33bf232fccd2e5ec34868a6160e72f8496a11f4e
saisbushigampala7 <sbushigampala1@cub.uca.edu>
2023-04-18 13:27:52 -0500

    just added one line.

--- ClientGUI/BoardPanel.java
+++ ClientGUI/BoardPanel.java
@@ -28,6 +28,22 @@ public class BoardPanel extends JPanel{
 		right2.add(resignButton);
 		bottom.add(right2, FlowLayout.RIGHT);
 		
+		JPanel top = new JPanel(new FlowLayout());
+		mainPanel.add(top, BorderLayout.NORTH);
+		
+		JPanel left1 = new JPanel(new FlowLayout());
+		left1.add(alertLabel, FlowLayout.LEFT);
+		left1.add(alertButton, FlowLayout.RIGHT);
+		top.add(left1, FlowLayout.LEFT);
+		
+		JPanel right1 = new JPanel(new FlowLayout());
+		right1.add(clockLabel, FlowLayout.LEFT);
+		// right1.add(clock, FlowLayout.RIGHT);
+		top.add(right1, FlowLayout.RIGHT);
+		
+		mainPanel.setVisible(true);
+		
+		
 		
 	}
 	
